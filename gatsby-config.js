5472acba5317e849526067a99c0f3c263266b414
Justin Kim <justinkim7395@gmail.com>
2021-05-05 13:56:42 -0700

    rough draft

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,52 +1,34 @@
 module.exports = {
-  siteMetadata: {
-    siteTitle: `Rocket Docs`,
-    defaultTitle: `Rocket Docs`,
-    siteTitleShort: `Rocket Docs`,
-    siteDescription: `Out of the box Gatsby Theme for creating documentation websites easily and quickly`,
-    siteUrl: `https://rocketdocs.netlify.app`,
-    siteAuthor: `@rocketseat`,
-    siteImage: `/banner.png`,
-    siteLanguage: `en`,
-    themeColor: `#8257E6`,
-    basePath: `/`,
-  },
-  flags: { PRESERVE_WEBPACK_CACHE: true },
-  plugins: [
-    {
-      resolve: `@rocketseat/gatsby-theme-docs`,
-      options: {
-        configPath: `src/config`,
-        docsPath: `src/docs`,
-        repositoryUrl: `https://github.com/rocketseat/gatsby-themes`,
-        baseDir: `examples/gatsby-theme-docs`,
-      },
+    siteMetadata: {
+        siteTitle: `hs.js`,
+        defaultTitle: `hs.js`,
+        siteTitleShort: `hs.js`,
+        siteDescription: `A Web Development Course for High Schoolers`,
+        siteUrl: `https://hsjs.netlify.app`,
+        siteAuthor: `@justinsunho`,
+        siteLanguage: `en`,
+        themeColor: `#8257E6`,
+        basePath: `/`,
     },
-    {
-      resolve: `gatsby-plugin-manifest`,
-      options: {
-        name: `Rocketseat Gatsby Themes`,
-        short_name: `RS Gatsby Themes`,
-        start_url: `/`,
-        background_color: `#ffffff`,
-        display: `standalone`,
-        icon: `static/favicon.png`,
-      },
-    },
-    `gatsby-plugin-sitemap`,
-    // {
-    //   resolve: `gatsby-plugin-google-analytics`,
-    //   options: {
-    //     trackingId: `YOUR_ANALYTICS_ID`,
-    //   },
-    // },
-    `gatsby-plugin-remove-trailing-slashes`,
-    {
-      resolve: `gatsby-plugin-canonical-urls`,
-      options: {
-        siteUrl: `https://rocketdocs.netlify.app`,
-      },
-    },
-    `gatsby-plugin-offline`,
-  ],
+    flags: { PRESERVE_WEBPACK_CACHE: true },
+    plugins: [
+        {
+            resolve: `@rocketseat/gatsby-theme-docs`,
+            options: {
+                configPath: `src/config`,
+                docsPath: `src/docs`,
+                repositoryUrl: `https://github.com/rocketseat/gatsby-themes`,
+                baseDir: `examples/gatsby-theme-docs`,
+            },
+        },
+        `gatsby-plugin-sitemap`,
+        `gatsby-plugin-remove-trailing-slashes`,
+        {
+            resolve: `gatsby-plugin-canonical-urls`,
+            options: {
+                siteUrl: `https://hsjs.netlify.app`,
+            },
+        },
+        `gatsby-plugin-offline`,
+    ],
 };
